00bebcfa85e92e8ba5884f326b5467a9110ba690
MasanobuRyuman <22011376mr@tama.ac.jp>
2022-10-22 20:56:55 +0900

    openapiのファイルを変更

--- backend/src/infrastructure/server/main.go
+++ backend/src/infrastructure/server/main.go
@@ -11,21 +11,13 @@ package main
 
 import (
 	"log"
-
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//sw "github.com/GIT_USER_ID/GIT_REPO_ID/go"
-	//
-	sw "./go"
+	"everybodys_refactoring/src/infrastructure/server/router"
 )
 
 func main() {
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+	router := router.NewRouter()
 
 	log.Fatal(router.Run(":8080"))
 }
